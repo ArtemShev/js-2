3beb02a653b4d629d3ebac6629ce9019c482d839
Artem Shevelev <Artem1990shevelev@gmail.com>
2021-05-20 15:38:42 +0300

    lesoon-2

--- script.js
+++ script.js
@@ -1,15 +1,64 @@
-const goods = [
-    { title: "Shirt", price: 150, img: "https://picsum.photos/200/150" },
-    { title: "Socks", price: 50, img: "https://picsum.photos/200/150" },
-    { title: "Jacket", price: 350, img: "https://picsum.photos/200/150" },
-    { title: "Shoes", price: 250, img: "https://picsum.photos/200/150" },
-];
+class GoodsItem {
+    constructor(title, price, img) {
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="goods-item"><img class="img-item" src=${this.img}><h3 class="title">${this.title}</h3><p class="price">Цена: ${this.price}</p> <button class="add">Добавить</button></div>`;
+    }
+}
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: "Shirt", price: 150, img: "https://picsum.photos/200/150" },
+            { title: "Socks", price: 50, img: "https://picsum.photos/200/150" },
+            { title: "Jacket", price: 350, img: "https://picsum.photos/200/150" },
+            { title: "Shoes", price: 250, img: "https://picsum.photos/200/150" },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price, good.img);
+            listHtml += goodItem.render()
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
 
-const renderGoodsItem = (title = "Название товара не найдено", price = "Цена не найдена", img = "картинка не найдена") => `<div class="goods-item"><img class="img-item" src=${img}><h3 class="title">${title}</h3><p class="price">Цена: ${price}</p> <button class="add">Добавить</button></div>`;
-const renderGoodsList = (list = []) => {
-    let html = "";
-    list.forEach(item => html += renderGoodsItem(item.title, item.price, item.img));
-    document.querySelector('.goods-list').innerHTML = html;
+    }
+    fullSum() {
+        let sum = 0;
+        this.goods.forEach(good => sum += good.price);
+        console.log(sum);
+
+    }
+
+}
+class CartItem extends GoodsItem {
+    constructor(...args) {
+        super(...args);
+        this.count = 0;
+    }
+    addOne() {};
+    removeOne() {};
+
+
+}
+class Cart extends GoodsList {
+    constructor(...args) {
+        super(...args);
+    }
+    clearCart() {};
+    removeItem() {};
+    addItem() {};
+    cartSum() {};
 }
 
-renderGoodsList(goods);
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.fullSum()
